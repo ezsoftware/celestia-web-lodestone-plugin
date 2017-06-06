e84d871a38aa9253edb7a35d9250386880a22bb0
ezsoftware <john@ezsoftwareinc.com>
2017-06-05 20:39:58 -0700

    convert hooks to static functions for daily cron per https://codex.wordpress.org/Function_Reference/register_activation_hook

--- classes/cron.daily.php
+++ classes/cron.daily.php
@@ -10,18 +10,16 @@ class CW_Cron_Daily {
     return self::$instance;
   }
   private function __construct() {
-    register_activation_hook(__FILE__, array($this, 'register_cron_hook'));
     add_action('cw_lodestone_daily_data_sync', array($this, 'do_daily_data_sync'));
-    register_deactivation_hook(__FILE__, array($this, 'deregister_cron_hook'));
   }
 
-  public function register_cron_hook() {
+  static function register_cron_hook() {
     if(!wp_next_scheduled('cw_lodestone_daily_event')) {
       wp_schedule_event(time(), 'daily', 'cw_lodestone_daily_data_sync');
     }
   }
 
-  public function deregister_cron_hook() {
+  static function deregister_cron_hook() {
     wp_clear_scheduled_hook('cw_lodestone_daily_data_sync');
   }
 
@@ -34,3 +32,7 @@ class CW_Cron_Daily {
     }
   }
 }
+
+
+register_activation_hook(__FILE__, array('CW_Cron_Daily', 'register_cron_hook'));
+register_deactivation_hook(__FILE__, array('CW_Cron_Daily', 'deregister_cron_hook'));
