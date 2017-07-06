f846410dbe9b28561ee1c13910a3aed35bbe2842
John Ryan <john@ezsoftwareinc.com>
2017-07-06 16:12:21 -0700

    member list logic

--- classes/members/list.php
+++ classes/members/list.php
@@ -27,6 +27,42 @@ class CW_LS_Members_List {
     add_shortcode("cw_ls_member_list", array($this, 'show_member_list'));
   }
 
+  function getMemberLink($member) {
+    return "";
+  }
+
+  function rankToValue($rank) {
+    switch(strtolower($rank)) {
+      case "king":
+        return 1;
+      case "queen":
+        return 2;
+      case "kings hand":
+        return 3;
+      case "magistrate":
+        return 4;
+      case "archduke":
+        return 5;
+      case "baron":
+        return 6;
+      case "knight":
+        return 7;
+      case "soldier":
+        return 8;
+      default:
+        return 9;
+    }
+  }
+
+  function cmp($a, $b)
+  {
+    $rank = self::rankToValue($a['rank']) - self::rankToValue($b['rank']);
+    if($rank === 0) {
+      return strcmp($a['name'], $b['name']);
+    }
+    return $rank;
+  }
+
   public function show_member_list() {
     $templates = self::getTemplates();
     
@@ -35,6 +71,14 @@ class CW_LS_Members_List {
       return "<h3>Invalid or Non-existant FC Membership Data</h3>";
     }
 
-    var_dump($memberList);
+    usort($memberList, array($this, 'cmp'));
+    $memberListItemHTML = "";
+    foreach($memberList as $index => $member) {
+      $listHtml = json_decode(json_encode($templates->listItem));
+      $listHtml = str_replace($member['character_id'], '{character_id}', $listHtml);
+      $listHtml = str_replace($member['name'], '{character_name}', $listHtml);
+      $listHtml = str_replace(self::getMemberLink($member), '{member_view_icon}', $listHtml);
+      $listHtml = str_replace($member['rank'], '{rank}', $listHtml);
+    }
   }
 }
